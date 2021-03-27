a9fff52a9c60f445e42c6ecf8c2ff3671809c8f8
John Rinehart <johnrichardrinehart@gmail.com>
2021-03-27 16:21:57 +0200

    feat: upload file with legal lock using S3 API

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/johnrichardrinehart/backblaze-terraform-backend-proxy
 
 go 1.16
+
+require github.com/aws/aws-sdk-go v1.38.7 // indirect
--- pkg/b2Storer.go
+++ pkg/b2Storer.go
@@ -2,15 +2,17 @@ package backend
 
 import (
 	"bytes"
-	"crypto/sha1"
 	"encoding/base64"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"net/url"
-	"strconv"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 const AUTH_URL = "https://api.backblazeb2.com/b2api/v2/b2_authorize_account"
@@ -21,7 +23,8 @@ type B2 struct {
 	APIUrl                       string // for API calls (not uploading/downloading)
 	Filename                     string // filename of the object to upload/download
 	FilenamePrefix               string // bucket-configured filename prefix (app key security restriction)
-	BucketID                     string // Bucket on which to perform operations
+	BucketName                   string // Bucket on which to perform operations
+	Client                       *s3.S3 // S3 Client
 	// DownloadURL                  string // for downloading files (upload URLs are obtained from get_upload_url)
 }
 
@@ -69,15 +72,31 @@ func NewB2(keyID, appKey, path string) (*B2, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error authorizing keyID %s: %s", keyID, err)
 	}
+	if authInfo.Allowed.BucketName == "" {
+		return nil, fmt.Errorf("failed to retrieve bucket name using application key: %s", keyID)
+	}
+
+	s3Config := &aws.Config{
+		Credentials:      credentials.NewStaticCredentials(keyID, appKey, ""),
+		Endpoint:         aws.String("https://s3.us-west-000.backblazeb2.com"),
+		Region:           aws.String("us-west-000"),
+		S3ForcePathStyle: aws.Bool(true),
+	}
+
+	newSession, err := session.NewSession(s3Config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create AWS session: %s", err)
+	}
+
+	s3Client := s3.New(newSession)
 
 	b2 := B2{
-		AccountAuthorizationAPIToken: authInfo.AuthorizationToken,
-		APIUrl:                       authInfo.ApiUrl,
-		Filename:                     path,
-		FilenamePrefix:               authInfo.Allowed.NamePrefix,
-		BucketID:                     authInfo.Allowed.BucketID,
-		// DownloadURL:                  authInfo.DownloadUrl,
+		Filename:       path,
+		FilenamePrefix: authInfo.Allowed.NamePrefix,
+		BucketName:     authInfo.Allowed.BucketName,
+		Client:         s3Client,
 	}
+
 	return &b2, nil
 }
 
@@ -85,93 +104,21 @@ func NewB2(keyID, appKey, path string) (*B2, error) {
 // 1. Get the URL for uploading
 // 2. Upload the datfa
 func (b2 B2) Store(bs []byte) error {
-	upURLInfo, err := b2.getUploadURL()
-	if err != nil {
-		return fmt.Errorf("failed to obtain upload URL: %s", err)
-	}
+	buf := bytes.NewReader(bs)
 
-	_, err = b2.uploadFile(upURLInfo.UploadAuthorizationToken, upURLInfo.UploadURL, bs)
+	bucketName := aws.String(b2.BucketName)
+	objectKey := aws.String(b2.FilenamePrefix + b2.Filename)
+
+	_, err := b2.Client.PutObject(&s3.PutObjectInput{
+		Body:                      buf,
+		Bucket:                    bucketName,
+		Key:                       objectKey,
+		ObjectLockLegalHoldStatus: aws.String(s3.ObjectLockLegalHoldStatusOn),
+	})
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to upload file, %v", err)
 	}
 	return nil
-}
-
-func (b2 B2) getUploadURL() (*responseGetUploadURL, error) {
-	// Define request
-	url, err := generateAPIPath(b2.APIUrl, API_VERSION, "get_upload_url")
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate API path for get_upload_url: %s", err)
-	}
-	buf := bytes.NewBuffer([]byte(fmt.Sprintf(`{"bucketId":"%s"}`, b2.BucketID)))
-	r, err := http.NewRequest(http.MethodPost, url, buf)
-	r.Header.Add("Authorization", b2.AccountAuthorizationAPIToken)
-	if err != nil {
-		return nil, fmt.Errorf("error creating get_upload_url request: %s", err)
-	}
-
-	// Execute Request
-	rsp, err := http.DefaultClient.Do(r)
-	if err != nil {
-		return nil, fmt.Errorf("error obtaining get_upload_url response: %s", err)
-	}
-
-	// Check for problems
-	if rsp.StatusCode != 200 {
-		bs, err := ioutil.ReadAll(rsp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("failed to dump response body: %s", err)
-		}
-		return nil, fmt.Errorf("B2 get_upload_url request failed with status code %d and message %s", rsp.StatusCode, bs)
-	}
-
-	defer rsp.Body.Close()
-
-	// JSON
-	var getUploadURL responseGetUploadURL
-	if err := json.NewDecoder(rsp.Body).Decode(&getUploadURL); err != nil {
-		return nil, fmt.Errorf("error obtaining URL for uploads: %s", err)
-	}
-	return &getUploadURL, nil
-}
-
-func (b2 B2) uploadFile(uploadToken, url string, data []byte) (*responseUploadFile, error) {
-	// Define Request
-	r, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
-	if err != nil {
-		return nil, fmt.Errorf("error creating auth request: %s", err)
-	}
-	r.Header.Add("Authorization", uploadToken)
-	r.Header.Add("X-Bz-File-Name", b2.FilenamePrefix+"/"+b2.Filename)
-	r.Header.Add("Content-Type", "application/octet-stream")
-	r.Header.Add("Content-Length", strconv.Itoa(len(data)))
-	h := sha1.New()
-	if _, err = h.Write(data); err != nil {
-		return nil, fmt.Errorf("failed to generated SHA-1 sum for upload file: %s", err)
-	}
-	r.Header.Add("X-Bz-Content-Sha1", hex.EncodeToString(h.Sum(nil)))
-
-	// Execute Request
-	rsp, err := http.DefaultClient.Do(r)
-	if err != nil {
-		return nil, fmt.Errorf("error obtaining upload file response: %s", err)
-	}
-
-	// Check for problems
-	if rsp.StatusCode != 200 {
-		bs, err := ioutil.ReadAll(rsp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("failed to dump response body: %s", err)
-		}
-		return nil, fmt.Errorf("B2 upload file request failed with status code %d and message %s", rsp.StatusCode, bs)
-	}
-
-	// JSON
-	var uploadInfo responseUploadFile
-	if err := json.NewDecoder(rsp.Body).Decode(&uploadInfo); err != nil {
-		return nil, fmt.Errorf("error obtaining upload file response info: %s", err)
-	}
-	return &uploadInfo, nil
 }
 
 func authorizeAccount(keyID, appKey string) (*responseAuthorizeAccount, error) {
@@ -206,16 +153,6 @@ func authorizeAccount(keyID, appKey string) (*responseAuthorizeAccount, error) {
 		return nil, fmt.Errorf("error obtaining authorization info: %s", err)
 	}
 
+	log.Printf("%+v", authInfo)
 	return &authInfo, nil
-}
-
-func generateAPIPath(base, version, endpoint string) (string, error) {
-	u, err := url.Parse(fmt.Sprintf("%s/b2api/%s/b2_%s", base, version, endpoint))
-	if err != nil {
-		return "", fmt.Errorf("unable to generate API path: %s", err)
-	}
-	if u.Scheme == "" {
-		u.Scheme = "https"
-	}
-	return u.String(), nil
 }
