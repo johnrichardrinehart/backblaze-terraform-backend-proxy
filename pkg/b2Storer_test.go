b0cabe30aea7aeabbe55aa5263dcdc7a71c4804d
John Rinehart <johnrichardrinehart@gmail.com>
2021-03-29 14:32:26 +0300

    test: remove test that leaves locked file

--- pkg/b2Storer_test.go
+++ pkg/b2Storer_test.go
@@ -29,36 +29,14 @@ func TestMain(m *testing.M) {
 	m.Run()
 }
 
-func TestLock(t *testing.T) {
-	tt := []struct {
-		name        string
-		expectError bool
-	}{
-		{
-			name:        "lock a test file",
-			expectError: false,
-		},
-		{
-			name:        "re-lock a test file",
-			expectError: false,
-		},
-	}
-
-	for _, tst := range tt {
-		err := b2.Lock()
-		if err != nil && !tst.expectError {
-			t.Errorf(`encountered unexpected error on TestLock test "%s": %s`, tst.name, err)
-		}
-		if err == nil && tst.expectError {
-			t.Errorf(`expected an error for TestLock test "%s" but none encountered`, tst.name)
-		}
-	}
-}
-
 // 1. Lock file
 // 2. Upload data
 // 3. Unlock file
 func TestCycle(t *testing.T) {
+	b2.Filename = "TestCycle"
+	if err := b2.Store(nil); err != nil {
+		t.Errorf("TestCycle failed to store the document: %s", err)
+	}
 	if err := b2.Lock(); err != nil {
 		t.Errorf("TestCycle failed to lock: %s", err)
 	}
